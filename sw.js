46568e9bac1ba3fa621c775e5e089f6892ef4ff7
kdev29 <leoc_2911@outlook.com>
2018-07-10 23:56:29 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/'
+];
+
+self.addEventListener('install', function(event){
+    console.log('Installing sw...');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
+      );
+    
+});
+
+self.addEventListener('fetch', function(event){
+    console.log('Fetch event fired');
+    console.log(event);
+
+    event.respondWith(
+        caches.match(event.request)
+          .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          }
+        )
+      );
+    
+});
+
+self.addEventListener('message',function(e){
+    self.postMessage('Hello from worker<- ' + e.data);
+});
